1387f9dc43834d4c723cf8dcb540b8667d29f21c
Gabriel <34012267+HyperPolygon64@users.noreply.github.com>
2019-05-07 23:30:55 +0100

    Added check for MSTs to decode

--- Sonic '06 Toolkit/Main.cs
+++ Sonic '06 Toolkit/Main.cs
@@ -1011,63 +1011,68 @@ namespace Sonic_06_Toolkit
 
         void Shortcuts_DecodeMSTs_Click(object sender, EventArgs e)
         {
-            try
+            //Checks if there are any MSTs in the directory.
+            if (Directory.GetFiles(Global.currentPath, "*.mst").Length == 0) MessageBox.Show("There are no MSTs to decode in this directory.", "No MSTs available", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
             {
-                Global.mstState = "decode";
-
-                UnicodeEncoding unicode = new UnicodeEncoding(true, false, false);
-                byte[] buffer = new byte[1024];
-
-                // arg0 is assumed to be a file to split
-                foreach (string MST in Directory.GetFiles(Global.currentPath, "*.mst", SearchOption.TopDirectoryOnly))
+                try
                 {
-                    List<string> results = new List<string>();          // <- BTW Hyper this array contains ALL the lines read from the file, including any bugged non-unicode lines.
+                    Global.mstState = "decode";
 
-                    using (FileStream fs = new FileStream(MST, FileMode.Open))
+                    UnicodeEncoding unicode = new UnicodeEncoding(true, false, false);
+                    byte[] buffer = new byte[1024];
+
+                    // arg0 is assumed to be a file to split
+                    foreach (string MST in Directory.GetFiles(Global.currentPath, "*.mst", SearchOption.TopDirectoryOnly))
                     {
-                        byte[] strbuf = new byte[1024];
-                        int len = 0, pos = 0;
+                        List<string> results = new List<string>();          // <- BTW Hyper this array contains ALL the lines read from the file, including any bugged non-unicode lines.
 
-                        while ((len = fs.Read(buffer, 0, buffer.Length)) > 0)
+                        using (FileStream fs = new FileStream(MST, FileMode.Open))
                         {
-                            for (int i = 0; i < len; i += 2)
+                            byte[] strbuf = new byte[1024];
+                            int len = 0, pos = 0;
+
+                            while ((len = fs.Read(buffer, 0, buffer.Length)) > 0)
                             {
-
-                                // check if the next 2 bytes are an end token. Check second byte last because its most likely 0 anyway but we gotta be sure...
-                                if ((buffer[i + 1] == 0 || buffer[i + 1] == 0xA) && buffer[i] == 0)
+                                for (int i = 0; i < len; i += 2)
                                 {
-                                    // this is a null terminator or a newline
-                                    results.Add(unicode.GetString(strbuf, 0, pos));
-                                    pos = 0;
 
-                                }
-                                else
-                                {
-                                    // if the buffer we allocated is too small, reallocate a larger array...
-                                    if (pos >= strbuf.Length)
+                                    // check if the next 2 bytes are an end token. Check second byte last because its most likely 0 anyway but we gotta be sure...
+                                    if ((buffer[i + 1] == 0 || buffer[i + 1] == 0xA) && buffer[i] == 0)
                                     {
-                                        byte[] _sbuf = strbuf;
-                                        strbuf = new byte[_sbuf.Length << 1];
-                                        Array.Copy(_sbuf, strbuf, _sbuf.Length);
-                                    }
+                                        // this is a null terminator or a newline
+                                        results.Add(unicode.GetString(strbuf, 0, pos));
+                                        pos = 0;
 
-                                    // copy next 2 bytes into strbuffer... If file size is not divisible by 2, this is gonna be a real problem here
-                                    strbuf[pos++] = buffer[i];
-                                    strbuf[pos++] = buffer[i + 1];
+                                    }
+                                    else
+                                    {
+                                        // if the buffer we allocated is too small, reallocate a larger array...
+                                        if (pos >= strbuf.Length)
+                                        {
+                                            byte[] _sbuf = strbuf;
+                                            strbuf = new byte[_sbuf.Length << 1];
+                                            Array.Copy(_sbuf, strbuf, _sbuf.Length);
+                                        }
+
+                                        // copy next 2 bytes into strbuffer... If file size is not divisible by 2, this is gonna be a real problem here
+                                        strbuf[pos++] = buffer[i];
+                                        strbuf[pos++] = buffer[i + 1];
+                                    }
                                 }
                             }
+
+                            // if the last string is not terminated, print it anyway
+                            if (pos > 0) results.Add(unicode.GetString(strbuf, 0, pos));
                         }
 
-                        // if the last string is not terminated, print it anyway
-                        if (pos > 0) results.Add(unicode.GetString(strbuf, 0, pos));
+                        File.WriteAllLines(Global.currentPath + Path.GetFileNameWithoutExtension(MST) + ".txt", results);
+
+                        Global.mstState = null;
                     }
-
-                    File.WriteAllLines(Global.currentPath + Path.GetFileNameWithoutExtension(MST) + ".txt", results);
-
-                    Global.mstState = null;
                 }
+                catch { MessageBox.Show("An error occurred when decoding the MSTs.", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
             }
-            catch { MessageBox.Show("An error occurred when decoding the MSTs.", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
         }
 
         void Shortcuts_ConvertSETs_Click(object sender, EventArgs e)
