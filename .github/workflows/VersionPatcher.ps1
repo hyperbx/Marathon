4eb69a8d825a903fc28c8671968bf3c339ab1d09
Hyper <HyperPolygon64@gmail.com>
2021-10-31 23:25:23 +0000

    [CLI] Actions: Version Patcher script

---
+++ .github/workflows/VersionPatcher.ps1
@@ -0,0 +1,29 @@
+param([String]$projectPath, [String]$version, [String]$commitID, [Boolean]$useFullCommitID)
+
+$project = [System.IO.File]::ReadAllLines($projectPath)
+
+if (!$useFullCommitID)
+{
+    $commitID = $commitID.Substring(0, 7)
+}
+
+$i = 0
+foreach ($line in $project)
+{
+    $lineSplit = $line.Split(">")
+
+    if ($lineSplit[0].Contains("<Version"))
+    {
+        $lineSplit[1] = "${version}</Version"
+    }
+    elseif ($lineSplit[0].Contains("<InformationalVersion"))
+    {
+        $lineSplit[1] = "${version}-${commitID}</InformationalVersion"
+    }
+
+    $project[$i] = [System.String]::Join(">", $lineSplit)
+
+    $i++
+}
+
+[System.IO.File]::WriteAllLines($projectPath, $project)
