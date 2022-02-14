40ba433a140260a347e401e1b2e1a5ba5fd1e359
Hyper <34012267+HyperBE32@users.noreply.github.com>
2022-02-14 18:48:57 +0000

    Use tabs so ReSharper doesn't break indentation (hopefully) [skip ci]

--- Marathon/Helpers/StringHelper.cs
+++ Marathon/Helpers/StringHelper.cs
@@ -2,39 +2,39 @@
 
 namespace Marathon.Helpers
 {
-    public class StringHelper
-    {
-        /// <summary>
-        /// Returns the full extension from the file name (with a dot prefix).
-        /// </summary>
-        public static string GetFullExtension(string fileName)
-            => Regex.Match(Path.GetFileName(fileName), @"\..*").Value;
+	public class StringHelper
+	{
+		/// <summary>
+		/// Returns the full extension from the file name (with a dot prefix).
+		/// </summary>
+		public static string GetFullExtension(string fileName)
+			=> Regex.Match(Path.GetFileName(fileName), @"\..*").Value;
 
-        /// <summary>
-        /// Returns the file name completely extension-less.
-        /// </summary>
-        public static string RemoveFullExtension(string fileName)
-            => Regex.Replace(Path.GetFileName(fileName), @"\..*", string.Empty);
+		/// <summary>
+		/// Returns the file name completely extension-less.
+		/// </summary>
+		public static string RemoveFullExtension(string fileName)
+			=> Regex.Replace(Path.GetFileName(fileName), @"\..*", string.Empty);
 
-        /// <summary>
-        /// Appends text to the file name before the extension.
-        /// </summary>
-        /// <param name="filePath">Full file path.</param>
-        /// <param name="text">Text to append.</param>
-        public static string AppendToFileName(string filePath, string text)
-            => Path.GetFileNameWithoutExtension(filePath) + text + GetFullExtension(filePath);
+		/// <summary>
+		/// Appends text to the file name before the extension.
+		/// </summary>
+		/// <param name="filePath">Full file path.</param>
+		/// <param name="text">Text to append.</param>
+		public static string AppendToFileName(string filePath, string text)
+			=> Path.GetFileNameWithoutExtension(filePath) + text + GetFullExtension(filePath);
 
-        /// <summary>
-        /// Returns a new path with the specified filename.
-        /// </summary>
-        public static string ReplaceFilename(string path, string newFile)
-            => Path.Combine(Path.GetDirectoryName(path), Path.GetFileName(newFile));
+		/// <summary>
+		/// Returns a new path with the specified filename.
+		/// </summary>
+		public static string ReplaceFilename(string path, string newFile)
+			=> Path.Combine(Path.GetDirectoryName(path), Path.GetFileName(newFile));
 
-        /// <summary>
-        /// Parses all line breaks from a string and returns an array of lines.
-        /// </summary>
-        /// <param name="input">String to parse line breaks from.</param>
-        public static string[] ParseLineBreaks(string input)
+		/// <summary>
+		/// Parses all line breaks from a string and returns an array of lines.
+		/// </summary>
+		/// <param name="input">String to parse line breaks from.</param>
+		public static string[] ParseLineBreaks(string input)
 			=> input.Split(new[] { '\r', '\n' });
-    }
+	}
 }
