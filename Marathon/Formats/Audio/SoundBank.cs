78c26f4b5e11ae39e7974d0866d76ecc0339360a
Hyper <34012267+hyperbx@users.noreply.github.com>
2025-07-08 17:52:11 +0100

    Refactored SoundBank

--- Marathon/Formats/Audio/SoundBank.cs
+++ Marathon/Formats/Audio/SoundBank.cs
@@ -1,211 +1,197 @@
-﻿namespace Marathon.Formats.Audio
+﻿using Marathon.IO;
+using Marathon.IO.Extensions;
+using Marathon.IO.Types.BINA;
+using System.Collections.Generic;
+using System.IO;
+
+// Format research attribution: Knuxfan24, Hyper
+
+namespace Marathon.Formats.Audio
 {
     /// <summary>
-    /// File base for the *.sbk format.
-    /// <para>Used in SONIC THE HEDGEHOG for defining properties for <a href="https://www.criware.com/">CriWare</a> sound effects in <see cref="CueSheetBinary"/> files.</para>
+    /// Support for *.sbk files; used for defining properties for <a href="https://www.criware.com/">CRIWARE</a> cues in cue sheet binaries (*.csb files).
     /// </summary>
     public class SoundBank : FileBase
     {
+        private const string _signature = "SBNK"; // "Sound BaNK"
+
         public SoundBank() { }
 
-        public SoundBank(string file, bool serialise = false)
+        public SoundBank(string in_path) : base(in_path) { }
+
+        /// <summary>
+        /// The name of this sound bank.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The sound cues in this bank.
+        /// </summary>
+        public List<Cue> Cues { get; set; } = [];
+
+        public Cue this[string in_name]
         {
-            switch (Path.GetExtension(file))
-            {
-                case ".json":
-                {
-                    Data = JsonDeserialise<FormatData>(file);
-
-                    // Save extension-less JSON (exploiting .NET weirdness, because it doesn't omit all extensions).
-                    if (serialise)
-                        Save(Path.GetFileNameWithoutExtension(file));
-
-                    break;
-                }
-
-                default:
-                {
-                    Load(file);
-
-                    if (serialise)
-                        JsonSerialise(Data);
-
-                    break;
-                }
-            }
+            get => Cues.Find((x) => x.Name == in_name);
         }
 
-        public override string Signature { get; } = "SBNK";
-
-        public override string Extension { get; } = ".sbk";
-
-        public class FormatData
+        public override void Read(Stream in_stream)
         {
-            public string Name { get; set; }
+            var reader = new BINAReader(in_stream);
 
-            public List<Cue> Cues = [];
+            reader.CheckSignature(_signature);
 
-            public override string ToString() => Name;
-        }
+            var unkField1 = reader.Read<uint>();         // TODO: unknown, always seems to be 0x20060700.
+            var nameOffset = reader.Read<uint>();        // Pointer to the sound bank name.
+            var cueTableOffset = reader.Read<uint>();    // Pointer to the first entry in the sound bank.
+            var cueIndicesOffset = reader.Read<uint>();  // Pointer to the index list for non-stream indices (null if none).
+            var streamTableOffset = reader.Read<uint>(); // Pointer to the pointer list for external stream paths (null if none).
 
-        public FormatData Data { get; set; } = new();
+            Name = reader.ReadStringFixedLength(0x40);
 
-        public override void Load(Stream stream)
-        {
-            BINAReader reader = new(stream);
+            var cueCount = reader.Read<uint>();       // Total number of cues in this sound bank.
+            var csbCueCount = reader.Read<uint>();    // Total number of cues in this sound bank which pull their data from a corresponding *.csb file.
+            var streamCueCount = reader.Read<uint>(); // Total number of cues in this sound bank which use external streams.
 
-            reader.ReadSignature(4, Signature);
-
-            uint UnknownUInt32_1   = reader.ReadUInt32(); // These four bytes seems to always be { 20, 06, 07, 00 } in official files.
-            uint bankNameOffset    = reader.ReadUInt32(); // Offset to the Sound Bank name (seemingly always { 00, 00, 00, 18 } in official files).
-            uint cueNameOffset     = reader.ReadUInt32(); // Offset of the first entry in the Sound Bank (seemingly always { 00, 00, 00, 64 } in official files).
-            uint cueIndiciesOffset = reader.ReadUInt32(); // Offset to the number list for non-stream indices (set to { 00, 00, 00, 00 } if the file doesn't have any).
-            uint streamOffset      = reader.ReadUInt32(); // Offset to the table for XMA names (set to { 00, 00, 00, 00 } if the file doesn't have any).
-
-            Data.Name           = reader.ReadNullPaddedString(0x40); // Sound Bank's name.
-            uint cueCount       = reader.ReadUInt32();               // Total Number of Cues in this Sound Bank.
-            uint csbCueCount    = reader.ReadUInt32();               // Amount of Cues in this Sound Bank which pull their data from a corresponding CSB file.
-            uint streamCueCount = reader.ReadUInt32();               // Amount of Cues in this Sound Bank which use XMA files.
-
-            int streams = 0; // Keep track of which stream we're on so we know where to jump to in the XMA string table.
+            var currentStream = 0;
 
             for (int i = 0; i < cueCount; i++)
             {
-                Cue cue = new() { Name = reader.ReadNullPaddedString(0x20) };
-
-                uint cueType  = reader.ReadUInt32();
-                uint cueIndex = reader.ReadUInt32();
-
-                cue.Category      = reader.ReadUInt32();
-                cue.UnknownSingle = reader.ReadSingle();
-                cue.Radius        = reader.ReadSingle();
-
-                if (cueType == 1)
+                var cue = new Cue()
                 {
-                    // Store current position for later so we can jump back.
-                    long pos = reader.BaseStream.Position;
+                    Name = reader.ReadStringFixedLength(0x20)
+                };
 
-                    reader.JumpTo(streamOffset, true);
-                    reader.JumpAhead(4 * streams); // Jump ahead to the right offset for our Cue's XMA.
+                var isExternalStream = reader.Read<uint>() != 0;
+                var cueIndex = reader.Read<uint>();
 
-                    cue.Stream = reader.ReadNullTerminatedString(false, reader.ReadUInt32(), true); // Read the XMA's name for this Cue.
+                cue.Category = reader.Read<uint>();
+                cue.UnknownField1 = reader.Read<float>();
+                cue.Radius = reader.Read<float>();
 
-                    reader.JumpTo(pos); // Jump back to where we were.
+                if (isExternalStream)
+                {
+                    var pos = reader.Position;
 
-                    streams++;
+                    // Navigate to the correct pointer in the table.
+                    reader.JumpTo(BINAHeader.Size + streamTableOffset);
+                    reader.JumpAhead(4 * currentStream);
+
+                    var streamNameOffset = reader.ReadUInt32();
+
+                    reader.ReadAtOffset(BINAHeader.Size + streamNameOffset, () => cue.Stream = reader.ReadStringNullTerminated());
+                    reader.JumpTo(pos);
+
+                    currentStream++;
                 }
 
-                // Save Cue to list.
-                Data.Cues.Add(cue);
+                Cues.Add(cue);
             }
         }
 
-        public override void Save(Stream stream)
+        public override void Write(Stream in_stream)
         {
-            BINAWriter writer = new(stream);
+            var writer = new BINAWriter(in_stream);
 
-            // Determine amount of Cues that use a CSB and amount that use an XMA.
-            int csbCueCount    = 0;
-            int streamCueCount = 0;
+            var csbCueCount = 0;
+            var streamCueCount = 0;
 
-            for (int i = 0; i < Data.Cues.Count; i++)
+            for (int i = 0; i < Cues.Count; i++)
             {
-                if (string.IsNullOrEmpty(Data.Cues[i].Stream))
+                if (string.IsNullOrEmpty(Cues[i].Stream))
                     csbCueCount++;
                 else
                     streamCueCount++;
             }
-
-            writer.WriteSignature(Signature);
-
-            writer.Write(537265920); // Hardcoded as all official files seem to have this number in this position.
-
-            writer.AddOffset("banksOffset");
-            writer.AddOffset("cueNamesOffset");
-            writer.AddOffset("cueIndicesOffset");
-            writer.AddOffset("streamsOffset");
-
-            writer.FillOffset("banksOffset", true);
-
-            writer.WriteNullPaddedString(Data.Name, 0x40);
-            writer.Write(Data.Cues.Count);
+        
+            writer.WriteSignature(_signature);
+            writer.Write(0x20060700); // TODO: unknown.
+            writer.CreateNamedField("NameOffset");
+            writer.CreateNamedField("CueTableOffset");
+            writer.CreateNamedField("CueIndicesOffset");
+            writer.CreateNamedField("StreamTableOffset");
+        
+            writer.WriteNamedField("NameOffset", (uint)writer.Position - BINAHeader.Size);
+        
+            writer.WriteStringFixedLength(Name, 0x40);
+            writer.Write(Cues.Count);
             writer.Write(csbCueCount);
             writer.Write(streamCueCount);
 
-            // Cue Information
-            writer.FillOffset("cueNamesOffset", true);
+            writer.WriteNamedField("CueTableOffset", (uint)writer.Position - BINAHeader.Size);
 
-            // Keep track of the entry types and IDs.
-            int csbCueID    = 0;
-            int streamCueID = 0;
-
-            for (int i = 0; i < Data.Cues.Count; i++)
+            var csbCueID = 0;
+            var streamCueID = 0;
+        
+            for (int i = 0; i < Cues.Count; i++)
             {
-                writer.WriteNullPaddedString(Data.Cues[i].Name, 0x20);
+                writer.WriteStringFixedLength(Cues[i].Name, 0x20);
 
-                // Write a CSB-based entry.
-                if (string.IsNullOrEmpty(Data.Cues[i].Stream))
+                if (string.IsNullOrEmpty(Cues[i].Stream))
                 {
+                    // Write CSB entry.
                     writer.Write(0);
                     writer.Write(csbCueID);
                     csbCueID++;
                 }
-
-                // Write an XMA-based entry.
                 else
                 {
+                    // Write external stream entry.
                     writer.Write(1);
                     writer.Write(streamCueID);
                     streamCueID++;
                 }
-
-                writer.Write(Data.Cues[i].Category);
-                writer.Write(Data.Cues[i].UnknownSingle);
-                writer.Write(Data.Cues[i].Radius);
+        
+                writer.Write(Cues[i].Category);
+                writer.Write(Cues[i].UnknownField1);
+                writer.Write(Cues[i].Radius);
             }
 
-            // CSB Cue ID List (if any are present).
             if (csbCueCount != 0)
             {
-                writer.FillOffset("cueIndicesOffset", true);
-
+                writer.WriteNamedField("CueIndicesOffset", (uint)writer.Position - BINAHeader.Size);
+        
                 for (int i = 0; i < csbCueCount; i++)
                     writer.Write(i);
             }
 
-            // Stream Names (if any are present).
             if (streamCueCount != 0)
             {
-                writer.FillOffset("streamsOffset", true);
-
-                for (int i = 0; i < Data.Cues.Count; i++)
+                writer.WriteNamedField("StreamTableOffset", (uint)writer.Position - BINAHeader.Size);
+        
+                for (int i = 0; i < Cues.Count; i++)
                 {
-                    if (Data.Cues[i].Stream != null)
-                        writer.AddString($"streamOffset{i}", $"{Data.Cues[i].Stream}");
+                    if (string.IsNullOrEmpty(Cues[i].Stream))
+                        continue;
+
+                    writer.CreateStringField($"StreamOffset{i}", Cues[i].Stream);
                 }
             }
 
-            // Write the footer.
             writer.FinishWrite();
+        }
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 
     public class Cue
     {
         /// <summary>
-        /// Name of this Cue in the Sound Bank.
+        /// The name of this cue in the sound bank.
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// Uncertain what exactly this affects.
+        /// TODO: unknown.
         /// </summary>
         public uint Category { get; set; }
 
         /// <summary>
-        /// Unknown - possibly a flag, possibly unused?
+        /// TODO: unknown, possibly a flag or maybe unused?
         /// </summary>
-        public float UnknownSingle { get; set; }
+        public float UnknownField1 { get; set; }
 
         /// <summary>
         /// The distance this sound can be heard from.
@@ -213,17 +199,18 @@
         public float Radius { get; set; }
 
         /// <summary>
-        /// XMA this Cue uses, if null, assume it uses a CSB instead.
+        /// The name of the XMA this cue uses.
+        /// <para>Leave blank if using audio from a *.csb file.</para>
         /// </summary>
         public string Stream { get; set; }
 
         public Cue() { }
 
-        public Cue(string in_name, uint in_category, float in_unknownSingle, float in_radius, string in_stream)
+        public Cue(string in_name, uint in_category, float in_unknownField1, float in_radius, string in_stream)
         {
             Name = in_name;
             Category = in_category;
-            UnknownSingle = in_unknownSingle;
+            UnknownField1 = in_unknownField1;
             Radius = in_radius;
             Stream = in_stream;
         }
