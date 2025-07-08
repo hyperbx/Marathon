96c9354394bcc110c7942036803fa343123d0b11
Hyper <34012267+hyperbx@users.noreply.github.com>
2025-07-08 17:51:52 +0100

    Refactored Collision

--- Marathon/Formats/Mesh/Collision.cs
+++ Marathon/Formats/Mesh/Collision.cs
@@ -1,215 +1,174 @@
 ﻿using Assimp;
 using Assimp.Configs;
+using Marathon.IO;
+using Marathon.IO.Extensions;
+using Marathon.IO.Types.BINA;
+using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Numerics;
+
+// Format research attribution: Darío, Knuxfan24, Hyper
 
 namespace Marathon.Formats.Mesh
 {
     /// <summary>
-    /// File base for the collision.bin format.
-    /// <para>Used in SONIC THE HEDGEHOG for collision meshes for terrain.</para>
+    /// Support for collision.bin files; used for collision meshes for terrain.
     /// </summary>
     public class Collision : FileBase
     {
         public Collision() { }
 
-        public Collision(string file, bool serialise = false)
+        public Collision(string in_path) : base(in_path) { }
+
+        public List<Vector3> Vertices { get; set; } = [];
+
+        public List<CollisionFace> Faces { get; set; } = [];
+
+        public override void Read(Stream in_stream)
         {
-            switch (Path.GetExtension(file))
-            {
-                case ".obj":
-                case ".fbx":
-                case ".dae":
-                {
-                    ImportAssimp(file);
+            var reader = new BINAReader(in_stream);
 
-                    // Save extension-less OBJ (exploiting .NET weirdness, because it doesn't omit all extensions).
-                    if (serialise)
-                        Save(Path.GetFileNameWithoutExtension(file));
+            var unkField1 = reader.Read<uint>(); // TODO: unknown.
+            var moppCodeOffset = reader.Read<uint>();
+            var vertexTableOffset = reader.Read<uint>();
+            var faceTableOffset = reader.Read<uint>();
+            var vertexCount = reader.Read<uint>();
 
-                    break;
-                }
-
-                default:
-                {
-                    Load(file);
-
-                    if (serialise)
-                        ExportOBJ($"{Location}.obj");
-
-                    break;
-                }
-            }
-        }
-
-        public override string Extension { get; } = ".bin";
-
-        public class FormatData
-        {
-            public List<Vector3> Vertices = [];
-            public List<CollisionFace> Faces = [];
-        }
-
-        public FormatData Data { get; set; } = new();
-
-        public override void Load(Stream stream)
-        {
-            BINAReader reader = new(stream);
-
-            uint unknownUInt32_1   = reader.ReadUInt32(); // TODO: Unknown, is apparently an offset?
-            uint moppCodeOffset    = reader.ReadUInt32(); // Offset to this file's Havok MOPP Code, which '06 doesn't seem to use?
-            uint vertexTableOffset = reader.ReadUInt32(); // Offset to this file's Vertex Table.
-            uint faceTableOffset   = reader.ReadUInt32(); // Offset to this file's Face Table.
-
-            reader.JumpTo(vertexTableOffset, true); // Should already be here but just to be safe.
-            uint vertexCount = reader.ReadUInt32(); // Get the amount of Vertices in this file.
-
-            // Read the Vertices.
             for (int i = 0; i < vertexCount; i++)
-                Data.Vertices.Add(reader.ReadVector3());
+                Vertices.Add(reader.Read<Vector3>());
 
-            reader.JumpTo(faceTableOffset, true); // Should already be here but just to be safe.
-            uint faceCount = reader.ReadUInt32(); // Get the amount of Faces in this file.
+            var faceCount = reader.Read<uint>();
 
-            // Read the Faces.
             for (int i = 0; i < faceCount; i++)
             {
                 CollisionFace face = new()
                 {
-                    VertexA = reader.ReadUInt16(),
-                    VertexB = reader.ReadUInt16(),
-                    VertexC = reader.ReadUInt16()
+                    VertexA = reader.Read<ushort>(),
+                    VertexB = reader.Read<ushort>(),
+                    VertexC = reader.Read<ushort>()
                 };
 
-                reader.FixPadding();
+                reader.Align(4);
 
-                face.Flags = (CollisionFlag)reader.ReadUInt32(); // TODO: Experiment with flags to see what they all do.
+                face.Flags = (CollisionFlag)reader.Read<uint>();
 
-                Data.Faces.Add(face);
+                Faces.Add(face);
             }
         }
 
-        public override void Save(Stream stream)
+        public override void Write(Stream in_stream)
         {
-            BINAWriter writer = new(stream);
+            var writer = new BINAWriter(in_stream);
 
-            writer.AddOffset("unknownUInt32_1");
+            writer.CreateNamedField("UnknownField1");
 
-            writer.WriteNulls(4); // Ignore MOPP code offset.
+            /* Havok MOPP code can only be generated using the
+               SDK, but thankfully it's optional in this game,
+               so we don't write it here. */
+            writer.Write(0);
 
-            writer.FillOffset("unknownUInt32_1", true);
+            writer.WriteNamedField("UnknownField1", (uint)writer.Position - BINAHeader.Size);
+            writer.CreateNamedField("VertexTableOffset");
+            writer.CreateNamedField("FaceTableOffset");
+            writer.WriteNamedField("VertexTableOffset", (uint)writer.Position - BINAHeader.Size);
+            writer.Write(Vertices.Count);
 
-            writer.AddOffset("vertexCountOffset");
-            writer.AddOffset("faceCountOffset");
+            for (int i = 0; i < Vertices.Count; i++)
+                writer.Write(Vertices[i]);
 
-            writer.FillOffset("vertexCountOffset", true);
-            writer.Write(Data.Vertices.Count);
+            writer.WriteNamedField("FaceTableOffset", (uint)writer.Position - BINAHeader.Size);
+            writer.Write(Faces.Count);
 
-            for (int i = 0; i < Data.Vertices.Count; i++)
-                writer.Write(Data.Vertices[i]);
-
-            writer.FillOffset("faceCountOffset", true);
-            writer.Write(Data.Faces.Count);
-
-            for (int i = 0; i < Data.Faces.Count; i++)
+            for (int i = 0; i < Faces.Count; i++)
             {
-                writer.Write(Data.Faces[i].VertexA);
-                writer.Write(Data.Faces[i].VertexB);
-                writer.Write(Data.Faces[i].VertexC);
-                writer.FixPadding();
-                writer.Write((uint)Data.Faces[i].Flags);
+                writer.Write(Faces[i].VertexA);
+                writer.Write(Faces[i].VertexB);
+                writer.Write(Faces[i].VertexC);
+                writer.Align(4);
+                writer.Write((uint)Faces[i].Flags);
             }
 
             writer.FinishWrite();
         }
 
-        public void ExportOBJ(string filePath)
+        // FIXME: something is going terribly wrong here!!
+        public override void Import(string in_path)
         {
-            // Create a SteamWriter.
-            var obj = new StreamWriter(filePath);
+            var ctx = new AssimpContext();
+            var cfg = new KeepSceneHierarchyConfig(true);
 
-            // Vertices.
-            foreach (Vector3 vertex in Data.Vertices)
-                obj.WriteLine($"v {vertex.X} {vertex.Y} {vertex.Z}");
+            ctx.SetConfig(cfg);
 
-            // Faces.
-            // Get all used flags.
-            var flags = new List<CollisionFlag>();
+            var assimpScene = ctx.ImportFile(in_path, PostProcessSteps.PreTransformVertices);
 
-            foreach (CollisionFace face in Data.Faces)
+            foreach (var assimpMesh in assimpScene.Meshes)
             {
-                if (!flags.Contains(face.Flags))
-                    flags.Add(face.Flags);
-            }
+                var name = assimpMesh.Name;
 
-            // Write Faces.
-            foreach (uint flag in flags)
-            {
-                obj.WriteLine($"\ng {flag.ToString("x").PadLeft(8, '0')}");
-
-                foreach (CollisionFace face in Data.Faces)
-                {
-                    if ((uint)face.Flags == flag)
-                        obj.WriteLine($"f {face.VertexA + 1} {face.VertexB + 1} {face.VertexC + 1}");
-                }
-            }
-
-            // Close the StreamWriter.
-            obj.Close();
-        }
-
-        public void ImportAssimp(string filePath)
-        {
-            // Setup AssimpNet Scene.
-            AssimpContext assimpImporter = new();
-            KeepSceneHierarchyConfig config = new(true);
-            assimpImporter.SetConfig(config);
-            Scene assimpModel = assimpImporter.ImportFile(filePath, PostProcessSteps.PreTransformVertices);
-
-            // Loop through all meshes in the imported file.
-            foreach (Assimp.Mesh assimpMesh in assimpModel.Meshes)
-            {
-                // Mesh Tag.
-                uint meshNameTag = 0u;
-
-                // Assimp seems to attach Mesh to end of DAE imports, lets check for that instead.
-                // Also seems to have a habit of adding an _ if the name is a number, so factor that in too...
-                string name = assimpMesh.Name;
+                // Assimp suffixes "Mesh" to end of Collada meshes.
                 if (name.EndsWith("Mesh"))
-                    name = name.Remove(name.LastIndexOf('M'));
-                if (name.StartsWith('_'))
-                    name = name.Substring(1);
+                    name = name[..name.LastIndexOf('M')];
 
-                // Try read an @ sign for backwards compatibility.
-                if (name.Contains('@'))
-                {
-                    meshNameTag = ParseFlags(name[(name.LastIndexOf('@') + 1)..]);
-                }
-                else
-                {
-                    meshNameTag = ParseFlags(name);
-                }
+                // Assimp prefixes '_' if the name is a number.
+                name = name.TrimStart('_');
 
-                // Faces.
-                foreach (Face assimpFace in assimpMesh.Faces)
+                // Trim '@' for backwards compatibility.
+                var tag = ParseFlags(name.TrimStart('@'));
+
+                foreach (var assimpFace in assimpMesh.Faces)
                 {
-                    CollisionFace face = new()
+                    var face = new CollisionFace()
                     {
-                        VertexA = (ushort)(assimpFace.Indices[0] + Data.Vertices.Count),
-                        VertexB = (ushort)(assimpFace.Indices[1] + Data.Vertices.Count),
-                        VertexC = (ushort)(assimpFace.Indices[2] + Data.Vertices.Count),
-                        Flags = (CollisionFlag)meshNameTag
+                        VertexA = (ushort)(assimpFace.Indices[0] + Vertices.Count),
+                        VertexB = (ushort)(assimpFace.Indices[1] + Vertices.Count),
+                        VertexC = (ushort)(assimpFace.Indices[2] + Vertices.Count),
+                        Flags = (CollisionFlag)tag
                     };
 
-                    Data.Faces.Add(face);
+                    Faces.Add(face);
                 }
 
-                // Vertices.
-                foreach (Vector3D assimpVertex in assimpMesh.Vertices)
-                    Data.Vertices.Add(new Vector3(assimpVertex.X, assimpVertex.Y, assimpVertex.Z));
+                foreach (var assimpVertex in assimpMesh.Vertices)
+                    Vertices.Add(new Vector3(assimpVertex.X, assimpVertex.Y, assimpVertex.Z));
             }
         }
 
-        private uint ParseFlags(string in_str)
+        public override void Export(string in_path = "")
+        {
+            if (string.IsNullOrEmpty(in_path))
+                in_path = Location + ".obj";
+
+            var writer = new StreamWriter(in_path);
+
+            foreach (var vertex in Vertices)
+                writer.WriteLine($"v {vertex.X} {vertex.Y} {vertex.Z}");
+
+            var flags = new List<CollisionFlag>();
+
+            foreach (var face in Faces)
+            {
+                if (flags.Contains(face.Flags))
+                    continue;
+                
+                flags.Add(face.Flags);
+            }
+
+            foreach (var flag in flags)
+            {
+                writer.WriteLine($"\ng {flag.ToString("X").PadLeft(8, '0')}");
+
+                foreach (var face in Faces)
+                {
+                    if (face.Flags == flag)
+                        writer.WriteLine($"f {face.VertexA + 1} {face.VertexB + 1} {face.VertexC + 1}");
+                }
+            }
+
+            writer.Close();
+        }
+
+        private static uint ParseFlags(string in_str)
         {
             if (uint.TryParse(in_str, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var out_flags))
                 return out_flags;
@@ -238,7 +197,10 @@ namespace Marathon.Formats.Mesh
             Flags = in_flags;
         }
 
-        public override string ToString() => $"<{VertexA}, {VertexB}, {VertexC}> @{Flags.ToString("X").PadLeft(8, '0')}";
+        public override string ToString()
+        {
+            return $"<{VertexA}, {VertexB}, {VertexC}> @ 0x{Flags.ToString("X").PadLeft(8, '0')}";
+        }
     }
 
     public enum CollisionFlag : uint
