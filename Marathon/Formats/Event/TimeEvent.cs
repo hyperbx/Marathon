ea437df54e6151419699a9af891c1cea9f6b2898
Hyper <34012267+hyperbx@users.noreply.github.com>
2025-07-08 17:51:57 +0100

    Refactored TimeEvent

--- Marathon/Formats/Event/TimeEvent.cs
+++ Marathon/Formats/Event/TimeEvent.cs
@@ -1,168 +1,151 @@
-﻿namespace Marathon.Formats.Event
+﻿using Marathon.IO;
+using Marathon.IO.Extensions;
+using Marathon.IO.Types.BINA;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+// Format research attribution: Knuxfan24, Hyper
+
+namespace Marathon.Formats.Event
 {
     /// <summary>
-    /// File base for the *.tev format.
-    /// <para>Used in SONIC THE HEDGEHOG for defining timed events for objects.</para>
+    /// Support for *.tev files; used for timing audio and particle effects with Ninja animations.
     /// </summary>
     public class TimeEvent : FileBase
     {
+        private const string _signature = ".TEV"; // "Time EVent"
+
         public TimeEvent() { }
 
-        public TimeEvent(string file, bool serialise = false)
+        public TimeEvent(string in_path) : base(in_path) { }
+
+        /// <summary>
+        /// The reference animation file.
+        /// </summary>
+        public string Motion { get; set; }
+
+        public List<EventInfo> Events { get; set; } = [];
+
+        public override void Read(Stream in_stream)
         {
-            switch (Path.GetExtension(file))
+            var reader = new BINAReader(in_stream);
+
+            reader.CheckSignature(_signature);
+
+            // Always null.
+            reader.JumpAhead(4);
+
+            var motionPathOffset = reader.Read<uint>();
+
+            reader.ReadAtOffset(BINAHeader.Size + motionPathOffset, () => Motion = reader.ReadStringNullTerminated());
+
+            var eventCount = reader.Read<uint>();
+            var eventTableOffset = reader.Read<uint>();
+
+            for (int i = 0; i < eventCount; i++)
             {
-                case ".json":
-                {
-                    Data = JsonDeserialise<FormatData>(file);
+                var @event = new EventInfo();
 
-                    // Save extension-less JSON (exploiting .NET weirdness, because it doesn't omit all extensions).
-                    if (serialise)
-                        Save(Path.GetFileNameWithoutExtension(file));
+                // Always null.
+                reader.JumpAhead(4);
 
-                    break;
-                }
+                uint targetNodeOffset = reader.Read<uint>();
+                uint resourceFileOffset = reader.Read<uint>();
+                uint resourceNameOffset = reader.Read<uint>();
 
-                default:
-                {
-                    Load(file);
+                @event.StartTime = reader.Read<float>();
+                @event.EndTime = reader.Read<float>();
+                @event.UnknownField1 = reader.Read<uint>();
+                @event.Position = reader.Read<Vector3>();
+                @event.UnknownField2 = reader.Read<Vector3>();
 
-                    if (serialise)
-                        JsonSerialise(Data);
+                reader.ReadAtOffset(BINAHeader.Size + targetNodeOffset, () => @event.Target = reader.ReadStringNullTerminated());
+                reader.ReadAtOffset(BINAHeader.Size + resourceFileOffset, () => @event.Resource = reader.ReadStringNullTerminated());
+                reader.ReadAtOffset(BINAHeader.Size + resourceNameOffset, () => @event.ResourceName = reader.ReadStringNullTerminated());
 
-                    break;
-                }
+                Events.Add(@event);
             }
         }
 
-        public override string Signature { get; } = ".TEV";
-
-        public override string Extension { get; } = ".tev";
-
-        public class FormatData
+        public override void Write(Stream in_stream)
         {
-            public string Animation { get; set; }
+            var writer = new BINAWriter(in_stream);
+        
+            writer.WriteSignature(_signature);
+            writer.Write(0);
+            writer.CreateStringField("MotionNameOffset", Motion);
+            writer.Write(Events.Count);
+            writer.CreateNamedField("EventTableOffset");
+            writer.WriteNamedField("EventTableOffset", (uint)writer.Position - BINAHeader.Size);
 
-            public List<AnimationTimer> Events { get; set; } = [];
-
-            public override string ToString() => Animation;
-        }
-
-        public FormatData Data { get; set; } = new();
-
-        public override void Load(Stream stream)
-        {
-            BINAReader reader = new(stream);
-
-            reader.ReadSignature(4, Signature);
-
-            reader.JumpAhead(4); // Always null in official files.
-            uint XNMOffset = reader.ReadUInt32();
-
-            long position = reader.BaseStream.Position; // Save position so we can jump back.
-
-            Data.Animation = reader.ReadNullTerminatedString(false, XNMOffset, true); // Read XNM associated with this time event file.
-            reader.JumpTo(position);                                                  // Return to previous position.
-
-            uint EntryCount       = reader.ReadUInt32();
-            uint EventTableOffset = reader.ReadUInt32(); // Offset to the table of entries in this time event file.
-
-            // Jump to the Event Table's Offset for safety's sake, should always be ox20 really.
-            reader.JumpTo(EventTableOffset, true);
-
-            // Loop through entries
-            for (int i = 0; i < EntryCount; i++)
+            for (int i = 0; i < Events.Count; i++)
             {
-                AnimationTimer entry = new();
-
-                reader.JumpAhead(4); // Always null in official files.
-
-                uint TargetNodeOffset   = reader.ReadUInt32();
-                uint ParticleBankOffset = reader.ReadUInt32();
-                uint ParticleOffset     = reader.ReadUInt32();
-
-                entry.StartTime = reader.ReadSingle();
-                entry.EndTime = reader.ReadSingle();
-
-                reader.JumpAhead(1); // Always null in official files. Padding?
-
-                entry.UnknownBoolean = reader.ReadBoolean();
-
-                reader.JumpAhead(2); // Always null in official files. Padding?
-
-                entry.ParticlePosition  = reader.ReadVector3();
-                entry.UnknownVector3  = reader.ReadVector3();
-
-                // Store current position to jump back to for the next entry.
-                position = reader.BaseStream.Position;
-
-                reader.JumpTo(TargetNodeOffset, true);
-                entry.TargetNode = reader.ReadNullTerminatedString();
-
-                reader.JumpTo(ParticleBankOffset, true);
-                entry.ParticleContainer = reader.ReadNullTerminatedString();
-
-                reader.JumpTo(ParticleOffset, true);
-                entry.ParticleName = reader.ReadNullTerminatedString();
-
-                // Jump back to the saved position to read the next entry..
-                reader.JumpTo(position);
-
-                // Save event entry into the Entries list.
-                Data.Events.Add(entry);
-            }
-        }
-
-        public override void Save(Stream stream)
-        {
-            BINAWriter writer = new(stream);
-
-            writer.WriteSignature(Signature);
-            writer.WriteNulls(4);
-            writer.AddString("XNMOffset", Data.Animation);
-            writer.Write(Data.Events.Count);
-            writer.AddOffset("EntriesOffset");
-
-            // Write the event entries.
-            writer.FillOffset("EntriesOffset", true);
-            for (int i = 0; i < Data.Events.Count; i++)
-            {
-                writer.WriteNulls(4);
-                writer.AddString($"TargetNode{i}", Data.Events[i].TargetNode);
-                writer.AddString($"ParticleBank{i}", Data.Events[i].ParticleContainer);
-                writer.AddString($"Particle{i}", Data.Events[i].ParticleName);
-                writer.Write(Data.Events[i].StartTime);
-                writer.Write(Data.Events[i].EndTime);
-                writer.WriteNulls(1);
-                writer.Write(Data.Events[i].UnknownBoolean);
-                writer.WriteNulls(2);
-                writer.Write(Data.Events[i].ParticlePosition);
-                writer.Write(Data.Events[i].UnknownVector3);
+                writer.Write(0);
+                writer.CreateStringField($"TargetNode{i}", Events[i].Target);
+                writer.CreateStringField($"ResourceFile{i}", Events[i].Resource);
+                writer.CreateStringField($"ResourceName{i}", Events[i].ResourceName);
+                writer.Write(Events[i].StartTime);
+                writer.Write(Events[i].EndTime);
+                writer.Write(Events[i].UnknownField1);
+                writer.Write(Events[i].Position);
+                writer.Write(Events[i].UnknownField2);
             }
 
-            // Write the footer.
             writer.FinishWrite();
+        }
+
+        public override string ToString()
+        {
+            return Motion;
         }
     }
 
-    public class AnimationTimer
+    public class EventInfo
     {
-        public string TargetNode { get; set; }
+        /// <summary>
+        /// The name of the bone to target.
+        /// </summary>
+        public string Target { get; set; }
 
-        public string ParticleContainer { get; set; }
+        /// <summary>
+        /// The name of the resource file to load from.
+        /// </summary>
+        public string Resource { get; set; }
 
-        public string ParticleName { get; set; }
+        /// <summary>
+        /// The name of the resource to pair with this animation.
+        /// </summary>
+        public string ResourceName { get; set; }
 
+        /// <summary>
+        /// The time during the animation this event starts at.
+        /// </summary>
         public float StartTime { get; set; }
 
+        /// <summary>
+        /// The time during the animation this event ends at.
+        /// </summary>
         public float EndTime { get; set; }
 
-        public bool UnknownBoolean { get; set; }
+        /// <summary>
+        /// TODO: unknown.
+        /// </summary>
+        public uint UnknownField1 { get; set; }
 
-        public Vector3 ParticlePosition { get; set; }
+        /// <summary>
+        /// The position of this event relative to the object.
+        /// </summary>
+        public Vector3 Position { get; set; }
 
-        public Vector3 UnknownVector3 { get; set; }
+        /// <summary>
+        /// TODO: unknown.
+        /// </summary>
+        public Vector3 UnknownField2 { get; set; }
 
-        public override string ToString() => ParticleName;
+        public override string ToString()
+        {
+            return ResourceName;
+        }
     }
 }
