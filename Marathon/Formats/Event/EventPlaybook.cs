8c1206fefbaa11fbafe31d1c90e0157ddf7a542e
Hyper <34012267+hyperbx@users.noreply.github.com>
2025-07-08 17:52:04 +0100

    Refactored EventPlaybook

--- Marathon/Formats/Event/EventPlaybook.cs
+++ Marathon/Formats/Event/EventPlaybook.cs
@@ -1,159 +1,173 @@
-﻿namespace Marathon.Formats.Event
+﻿using Marathon.IO;
+using Marathon.IO.Extensions;
+using Marathon.IO.Types.BINA;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+// Format research attribution: Knuxfan24, Hyper
+
+namespace Marathon.Formats.Event
 {
     /// <summary>
-    /// File base for the *.epb format.
-    /// <para>Used in SONIC THE HEDGEHOG for cutscene properties.</para>
+    /// Support for *.epb files; used for defining cutscene properties.
     /// </summary>
     public class EventPlaybook : FileBase
     {
+        private const string _signature = ".EPB"; // "Event PlayBook"
+
         public EventPlaybook() { }
 
-        public EventPlaybook(string file, bool serialise = false)
-        {
-            switch (Path.GetExtension(file))
-            {
-                case ".json":
-                {
-                    Events = JsonDeserialise<List<Event>>(file);
-
-                    // Save extension-less JSON (exploiting .NET weirdness, because it doesn't omit all extensions).
-                    if (serialise)
-                        Save(Path.GetFileNameWithoutExtension(file));
-
-                    break;
-                }
-
-                default:
-                {
-                    Load(file);
-
-                    if (serialise)
-                        JsonSerialise(Events);
-
-                    break;
-                }
-            }
-        }
-
-        public override string Signature { get; }= ".EPB";
-
-        public override string Extension { get; } = ".epb";
+        public EventPlaybook(string in_path) : base(in_path) { }
 
         public List<Event> Events { get; set; } = [];
 
-        public override void Load(Stream stream)
+        public Event this[string in_name]
         {
-            BINAReader reader = new(stream);
+            get => Events.Find((x) => x.Name == in_name);
+        }
 
-            reader.ReadSignature(4, Signature);
+        public override void Read(Stream in_stream)
+        {
+            var reader = new BINAReader(in_stream);
 
-            uint UnknownUInt32_1  = reader.ReadUInt32(); // TODO: Figure out what this controls, as nulling it out broke all in game scenes.
-            uint EventCount       = reader.ReadUInt32(); // Number of Events in the Playbook.
-            uint EventTableOffset = reader.ReadUInt32(); // Offset to this table of Events in the Playbook.
+            reader.CheckSignature(_signature);
 
-            // Jump to the Event Table's Offset for safety's sake, should always be 0x30 really.
-            reader.JumpTo(EventTableOffset, true);
+            var unkField1 = reader.Read<uint>(); // TODO: unknown, breaks all cutscenes if null.
+            var eventCount = reader.Read<uint>();
+            var eventTableOffset = reader.Read<uint>();
 
-            // Loop through based on EventCount.
-            for (int i = 0; i < EventCount; i++)
+            reader.JumpTo(BINAHeader.Size + eventTableOffset);
+
+            for (int i = 0; i < eventCount; i++)
             {
                 Event @event = new();
-                uint eventNameOffset    = reader.ReadUInt32();  // Offset to this event's name.
-                uint eventFolderOffset  = reader.ReadUInt32();  // Offset to this event's folder.
-                @event.EventLength      = reader.ReadUInt32();  // The length of this event in frames.
-                @event.Position         = reader.ReadVector3(); // The position this event's objects are placed relative to.
-                @event.Rotation         = reader.ReadVector3(); // The rotation of this event's objects.
-                uint terrainOffset      = reader.ReadUInt32();  // Offset to this event's terrain.
-                uint sceneLuaOffset     = reader.ReadUInt32();  // Offset to this event's scene lua.
-                uint sceneBankOffset    = reader.ReadUInt32();  // Offset to this event's scene bank.
-                uint particleListOffset = reader.ReadUInt32();  // Offset to this event's particle list.
-                uint subtitleMSTOffset  = reader.ReadUInt32();  // Offset to this event's MST file.
 
-                // Store current position to jump back to for the next entry.
-                long position = reader.BaseStream.Position;
+                var nameOffset = reader.Read<uint>();
+                var folderOffset = reader.Read<uint>();
+                
+                @event.Length = reader.Read<uint>();
+                @event.Position = reader.Read<Vector3>();
+                @event.Rotation = reader.Read<Vector3>();
+                
+                var terrainOffset = reader.Read<uint>();
+                var sceneLuaOffset = reader.Read<uint>();
+                var sceneBankOffset = reader.Read<uint>();
+                var particleContainerOffset = reader.Read<uint>();
+                var subtitleMessageTableOffset = reader.Read<uint>();
 
-                // Read all the string values, needs to check if the offset isn't 0 as it reads the .EPB signature otherwise.
-                if (eventNameOffset != 0)
-                    @event.Name = reader.ReadNullTerminatedString(false, eventNameOffset, true);
+                var pos = reader.Position;
 
-                if (eventFolderOffset != 0)
-                    @event.Folder = reader.ReadNullTerminatedString(false, eventFolderOffset, true);
+                if (nameOffset != 0)
+                    reader.ReadAtOffset(BINAHeader.Size + nameOffset, () => @event.Name = reader.ReadStringNullTerminated());
+
+                if (folderOffset != 0)
+                    reader.ReadAtOffset(BINAHeader.Size + folderOffset, () => @event.Folder = reader.ReadStringNullTerminated());
 
                 if (terrainOffset != 0)
-                    @event.Terrain = reader.ReadNullTerminatedString(false, terrainOffset, true);
+                    reader.ReadAtOffset(BINAHeader.Size + terrainOffset, () => @event.Terrain = reader.ReadStringNullTerminated());
 
                 if (sceneLuaOffset != 0)
-                    @event.SceneLua = reader.ReadNullTerminatedString(false, sceneLuaOffset, true);
+                    reader.ReadAtOffset(BINAHeader.Size + sceneLuaOffset, () => @event.SceneParameters = reader.ReadStringNullTerminated());
 
                 if (sceneBankOffset != 0)
-                    @event.SoundBank = reader.ReadNullTerminatedString(false, sceneBankOffset, true);
+                    reader.ReadAtOffset(BINAHeader.Size + sceneBankOffset, () => @event.SoundBank = reader.ReadStringNullTerminated());
 
-                if (particleListOffset != 0)
-                    @event.ParticleContainer = reader.ReadNullTerminatedString(false, particleListOffset, true);
+                if (particleContainerOffset != 0)
+                    reader.ReadAtOffset(BINAHeader.Size + particleContainerOffset, () => @event.ParticleContainer = reader.ReadStringNullTerminated());
 
-                if (subtitleMSTOffset != 0)
-                    @event.SubtitleMessageTable = reader.ReadNullTerminatedString(false, subtitleMSTOffset, true);
+                if (subtitleMessageTableOffset != 0)
+                    reader.ReadAtOffset(BINAHeader.Size + subtitleMessageTableOffset, () => @event.Subtitles = reader.ReadStringNullTerminated());
 
-                // Jump back to the saved position to read the next event.
-                reader.JumpTo(position);
+                reader.JumpTo(pos);
 
-                // Save object entry into the Entries list.
                 Events.Add(@event);
             }
         }
 
-        public override void Save(Stream stream)
+        public override void Write(Stream in_stream)
         {
-            BINAWriter writer = new(stream);
+            var writer = new BINAWriter(in_stream);
 
-            writer.WriteSignature(Extension.ToUpper());
-            writer.Write(537265920u); // TODO: Unhardcode this once we figure out what it does.
+            writer.WriteSignature(_signature);
+            writer.Write(0x20060700); // TODO: unknown.
             writer.Write(Events.Count);
-            writer.AddOffset("EventTableOffset");
+            writer.CreateNamedField("EventTableOffset");
+            writer.WriteNamedField("EventTableOffset", (uint)writer.Position - BINAHeader.Size);
 
-            // Write the events.
-            writer.FillOffset("EventTableOffset", true);
             for (int i = 0; i < Events.Count; i++)
             {
-                writer.AddString($"Event{i}Name", Events[i].Name);
-                writer.AddString($"Event{i}Folder", Events[i].Folder);
-                writer.Write(Events[i].EventLength);
+                writer.CreateStringField($"Event{i}Name", Events[i].Name);
+                writer.CreateStringField($"Event{i}Folder", Events[i].Folder);
+                writer.Write(Events[i].Length);
                 writer.Write(Events[i].Position);
                 writer.Write(Events[i].Rotation);
-                writer.AddString($"Event{i}Terrain", Events[i].Terrain);
-                writer.AddString($"Event{i}SceneLua", Events[i].SceneLua);
-                writer.AddString($"Event{i}SceneBank", Events[i].SoundBank);
-                writer.AddString($"Event{i}ParticleList", Events[i].ParticleContainer);
-                writer.AddString($"Event{i}SubtitleMST", Events[i].SubtitleMessageTable);
+                writer.CreateStringField($"Event{i}Terrain", Events[i].Terrain);
+                writer.CreateStringField($"Event{i}SceneLua", Events[i].SceneParameters);
+                writer.CreateStringField($"Event{i}SceneBank", Events[i].SoundBank);
+                writer.CreateStringField($"Event{i}ParticleList", Events[i].ParticleContainer);
+                writer.CreateStringField($"Event{i}SubtitleMST", Events[i].Subtitles);
             }
 
-            // Write the footer.
             writer.FinishWrite();
         }
     }
 
     public class Event
     {
+        /// <summary>
+        /// The name of this event.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The location of this event's resources.
+        /// </summary>
         public string Folder { get; set; }
 
+        /// <summary>
+        /// The location of this event's terrain.
+        /// </summary>
         public string Terrain { get; set; }
 
-        public string SceneLua { get; set; }
+        /// <summary>
+        /// The location of this event's scene parameter script.
+        /// </summary>
+        public string SceneParameters { get; set; }
 
+        /// <summary>
+        /// The location of this event's sound bank.
+        /// </summary>
         public string SoundBank { get; set; }
 
+        /// <summary>
+        /// The location of this event's particle container.
+        /// </summary>
         public string ParticleContainer { get; set; }
 
-        public string SubtitleMessageTable { get; set; }
+        /// <summary>
+        /// The location of this event's message table for subtitles.
+        /// </summary>
+        public string Subtitles { get; set; }
 
-        public uint EventLength { get; set; }
+        /// <summary>
+        /// The length of this event in frames.
+        /// </summary>
+        public uint Length { get; set; }
 
+        /// <summary>
+        /// The position of this event's objects relative to the origin point.
+        /// </summary>
         public Vector3 Position { get; set; }
 
+        /// <summary>
+        /// The rotation of this event's objects.
+        /// </summary>
         public Vector3 Rotation { get; set; }
 
-        public override string ToString() => Name;
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
