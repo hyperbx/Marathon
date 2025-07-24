b5ee0694b0f5af940e92c231b65656420c1be7a7
Hyper <34012267+hyperbx@users.noreply.github.com>
2025-07-24 08:44:30 +0100

    Add HsonExtensions.ToJsonString()

---
+++ Marathon/Extensions/HsonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using libHSON;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace Marathon.Extensions
+{
+    public static class HsonExtensions
+    {
+        public static string ToJsonString(this Project in_project, ProjectWriteOptions in_hsonOptions = default, JsonWriterOptions in_jsonOptions = default)
+        {
+            using var ms = new MemoryStream();
+
+            in_project.Write(ms, jsonOptions: new() { Indented = true });
+
+            return Encoding.UTF8.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+    }
+}
