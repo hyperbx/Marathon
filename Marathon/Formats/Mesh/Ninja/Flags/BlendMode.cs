d933fa04ed602cef30f31c2673f3c27e14acad2b
Hyper <34012267+hyperbx@users.noreply.github.com>
2025-08-06 03:39:57 +0100

    Ninja: document more blend modes

--- Marathon/Formats/Mesh/Ninja/Flags/BlendMode.cs
+++ Marathon/Formats/Mesh/Ninja/Flags/BlendMode.cs
@@ -8,6 +8,8 @@ namespace Marathon.Formats.Mesh.Ninja.Flags
     [JsonConverter(typeof(StringEnumConverter))]
     public enum BlendMode : uint
     {
+        NNE_BLENDMODE_NONE = 0,
+        NNE_BLENDMODE_ADDITIVE = 0x01,
         NNE_BLENDMODE_SRCALPHA = 0x302,
         NNE_BLENDMODE_INVSRCALPHA = 0x303
     }
