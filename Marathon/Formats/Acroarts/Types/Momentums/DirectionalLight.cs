8826c40ca78d47f6d70421b74bc5ec088d029f51
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-03-15 22:34:14 +0000

    Implement DirectionalLight

---
+++ Marathon/Formats/Acroarts/Types/Momentums/DirectionalLight.cs
@@ -0,0 +1,32 @@
+﻿using Marathon.Formats.Acroarts.Chunks;
+using Marathon.IO;
+
+namespace Marathon.Formats.Acroarts.Types.Momentums
+{
+    public class DirectionalLight : IMomentumParamSet
+    {
+        public uint UnknownField { get; set; }
+
+        public DirectionalLight() { }
+
+        public DirectionalLight(BinaryObjectReaderEx in_reader, IChunk in_parentChunk = null)
+        {
+            Read(in_reader, in_parentChunk);
+        }
+
+        public void Read(BinaryObjectReaderEx in_reader, IChunk in_parentChunk = null)
+        {
+            UnknownField = in_reader.Read<uint>();
+        }
+
+        public void Write(BinaryObjectWriterEx in_writer, IChunk in_parentChunk = null)
+        {
+            in_writer.Write(UnknownField);
+        }
+
+        public uint GetParamCount()
+        {
+            return 1;
+        }
+    }
+}
