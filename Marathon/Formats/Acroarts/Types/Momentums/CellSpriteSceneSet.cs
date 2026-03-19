0f31a87721409d363f534d35bcbb748bc33d817b
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-03-19 19:51:01 +0000

    CellSpriteSceneSet: mapped SceneMotionName field

--- Marathon/Formats/Acroarts/Types/Momentums/CellSpriteSceneSet.cs
+++ Marathon/Formats/Acroarts/Types/Momentums/CellSpriteSceneSet.cs
@@ -6,13 +6,13 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
     {
         public string SceneName { get; set; }
 
-        public uint UnknownField1 { get; set; }
+        public string SceneMotionName { get; set; }
 
-        public int UnknownField2 { get; set; }
+        public int UnknownField1 { get; set; }
 
-        public float UnknownField3 { get; set; }
+        public float UnknownField2 { get; set; }
 
-        public uint UnknownField4 { get; set; }
+        public uint UnknownField3 { get; set; }
 
         public CellSpriteSceneSet() { }
 
@@ -24,24 +24,28 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
         public void Read(BinaryObjectReaderEx in_reader)
         {
             var sceneNameOffset = in_reader.Read<uint>();
-            UnknownField1 = in_reader.Read<uint>();
-            UnknownField2 = in_reader.Read<int>();
-            UnknownField3 = in_reader.Read<float>();
-            UnknownField4 = in_reader.Read<uint>();
+            var sceneMotionNameOffset = in_reader.Read<uint>();
+            UnknownField1 = in_reader.Read<int>();
+            UnknownField2 = in_reader.Read<float>();
+            UnknownField3 = in_reader.Read<uint>();
 
             in_reader.ReadAtOffset(in_reader.CalculateOffset(sceneNameOffset),
                 () => SceneName = MomentumString.Read(in_reader));
+
+            in_reader.ReadAtOffset(in_reader.CalculateOffset(sceneMotionNameOffset),
+                () => SceneMotionName = MomentumString.Read(in_reader));
         }
 
         public void Write(BinaryObjectWriterEx in_writer)
         {
             var sceneNameOffset = in_writer.Reserve<uint>();
+            var sceneMotionNameOffset = in_writer.Reserve<uint>();
             in_writer.Write(UnknownField1);
             in_writer.Write(UnknownField2);
             in_writer.Write(UnknownField3);
-            in_writer.Write(UnknownField4);
 
             MomentumString.Write(in_writer, SceneName, sceneNameOffset);
+            MomentumString.Write(in_writer, SceneMotionName, sceneMotionNameOffset);
         }
 
         public uint GetParamCount()
