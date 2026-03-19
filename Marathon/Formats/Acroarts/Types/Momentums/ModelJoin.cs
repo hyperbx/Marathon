e1100ee26f10cd981c228c8a13eb2318d131aeb0
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-03-19 16:48:38 +0000

    Implement ModelJoin

---
+++ Marathon/Formats/Acroarts/Types/Momentums/ModelJoin.cs
@@ -0,0 +1,44 @@
+﻿using Marathon.IO;
+
+namespace Marathon.Formats.Acroarts.Types.Momentums
+{
+    public class ModelJoin : IMomentumParamSet
+    {
+        public uint UnknownField1 { get; set; }
+
+        public uint UnknownField2 { get; set; }
+
+        public string NodeName { get; set; }
+
+        public ModelJoin() { }
+
+        public ModelJoin(BinaryObjectReaderEx in_reader)
+        {
+            Read(in_reader);
+        }
+
+        public void Read(BinaryObjectReaderEx in_reader)
+        {
+            UnknownField1 = in_reader.Read<uint>();
+            UnknownField2 = in_reader.Read<uint>();
+            var nodeNameOffset = in_reader.Read<uint>();
+
+            in_reader.ReadAtOffset(in_reader.CalculateOffset(nodeNameOffset),
+                () => NodeName = MomentumString.Read(in_reader));
+        }
+
+        public void Write(BinaryObjectWriterEx in_writer)
+        {
+            in_writer.Write(UnknownField1);
+            in_writer.Write(UnknownField2);
+            var nodeNameOffset = in_writer.Reserve<uint>();
+
+            MomentumString.Write(in_writer, NodeName, nodeNameOffset);
+        }
+
+        public uint GetParamCount()
+        {
+            return 3;
+        }
+    }
+}
--- Marathon/Formats/Acroarts/Types/Momentums/MomentumFactory.cs
+++ Marathon/Formats/Acroarts/Types/Momentums/MomentumFactory.cs
@@ -29,6 +29,7 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
                 MomentumType.TurnCamera => new TurnCamera(in_reader),
                 MomentumType.MaterialColorNormal => new MaterialColorNormal(in_reader),
                 MomentumType.MaterialColorGoal => new MaterialColorGoal(in_reader),
+                MomentumType.ModelJoin => new ModelJoin(in_reader),
                 MomentumType.ShadowOn => new ShadowOn(in_reader),
                 MomentumType.DirectionalLight => new DirectionalLight(in_reader),
                 MomentumType.MotionSet => new MotionSet(in_reader),
