caa6a742c9c5107f881a9c65815d6881bce7abed
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-03-20 14:52:04 +0000

    ParticleBillboardPV: rename unknown fields

--- Marathon/Formats/Acroarts/Types/Momentums/ParticleBillboardPV.cs
+++ Marathon/Formats/Acroarts/Types/Momentums/ParticleBillboardPV.cs
@@ -11,7 +11,7 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
     {
         public ParticleEmitterType EmitterType { get; set; }
 
-        public uint UnknownField2 { get; set; }
+        public uint UnknownField1 { get; set; }
 
         public Vector3 Position { get; set; }
 
@@ -25,7 +25,7 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
 
         public Distance<float> DistanceHeight { get; set; }
 
-        public uint UnknownField15 { get; set; }
+        public uint UnknownField2 { get; set; }
 
         public bool UseSplineMotion { get; set; }
 
@@ -41,9 +41,9 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
 
         public float Rate { get; set; }
 
-        public uint UnknownField23 { get; set; }
+        public uint UnknownField3 { get; set; }
 
-        public uint UnknownField24 { get; set; }
+        public uint UnknownField4 { get; set; }
 
         public float Angle { get; set; }
 
@@ -51,23 +51,23 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
 
         public bool Billboard { get; set; }
 
-        public uint UnknownField29 { get; set; }
+        public uint UnknownField5 { get; set; }
 
-        public uint UnknownField30 { get; set; }
+        public uint UnknownField6 { get; set; }
 
-        public uint UnknownField31 { get; set; }
+        public uint UnknownField7 { get; set; }
 
-        public uint UnknownField32 { get; set; }
+        public uint UnknownField8 { get; set; }
 
-        public uint UnknownField33 { get; set; }
+        public uint UnknownField9 { get; set; }
 
-        public uint UnknownField34 { get; set; }
+        public uint UnknownField10 { get; set; }
 
-        public uint UnknownField35 { get; set; }
+        public uint UnknownField11 { get; set; }
 
-        public float UnknownField36 { get; set; }
+        public float UnknownField12 { get; set; }
 
-        public float UnknownField37 { get; set; }
+        public float UnknownField13 { get; set; }
 
         public Distance<float> DistanceX { get; set; }
 
@@ -75,11 +75,11 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
 
         public Distance<float> DistanceZ { get; set; }
 
-        public uint UnknownField44 { get; set; }
+        public uint UnknownField14 { get; set; }
 
         public float Parameter { get; set; }
 
-        public uint UnknownField46 { get; set; }
+        public uint UnknownField15 { get; set; }
 
         public IndirectMomentumParamList<ColorKeyFrame> ColorKeyFrames { get; set; } = [];
 
@@ -103,7 +103,7 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
 
         public uint PatternCount { get; set; }
 
-        public bool UnknownField58 { get; set; }
+        public bool UnknownField16 { get; set; }
 
         public bool UseRandomPattern { get; set; }
 
@@ -117,14 +117,14 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
         public void Read(BinaryObjectReaderEx in_reader)
         {
             EmitterType = in_reader.Read<ParticleEmitterType>();
-            UnknownField2 = in_reader.Read<uint>();
+            UnknownField1 = in_reader.Read<uint>();
             Position = in_reader.Read<Vector3>();
             IsCameraLocal = in_reader.ReadBoolean<uint>();
             Radius = in_reader.Read<float>();
             Scale = in_reader.Read<Vector3>();
             DistanceWidth = in_reader.Read<Distance<float>>();
             DistanceHeight = in_reader.Read<Distance<float>>();
-            UnknownField15 = in_reader.Read<uint>();
+            UnknownField2 = in_reader.Read<uint>();
             UseSplineMotion = in_reader.ReadBoolean<uint>();
 
             var splineKeyFramesOffset = in_reader.Read<uint>();
@@ -154,26 +154,26 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
             BlendMode = in_reader.Read<int>();
             ParticleCount = in_reader.Read<uint>();
             Rate = in_reader.Read<float>();
-            UnknownField23 = in_reader.Read<uint>();
-            UnknownField24 = in_reader.Read<uint>();
+            UnknownField3 = in_reader.Read<uint>();
+            UnknownField4 = in_reader.Read<uint>();
             Angle = in_reader.Read<float>();
             DistanceAngle = in_reader.Read<Distance<float>>();
             Billboard = in_reader.ReadBoolean<uint>();
-            UnknownField29 = in_reader.Read<uint>();
-            UnknownField30 = in_reader.Read<uint>();
-            UnknownField31 = in_reader.Read<uint>();
-            UnknownField32 = in_reader.Read<uint>();
-            UnknownField33 = in_reader.Read<uint>();
-            UnknownField34 = in_reader.Read<uint>();
-            UnknownField35 = in_reader.Read<uint>();
-            UnknownField36 = in_reader.Read<float>();
-            UnknownField37 = in_reader.Read<float>();
+            UnknownField5 = in_reader.Read<uint>();
+            UnknownField6 = in_reader.Read<uint>();
+            UnknownField7 = in_reader.Read<uint>();
+            UnknownField8 = in_reader.Read<uint>();
+            UnknownField9 = in_reader.Read<uint>();
+            UnknownField10 = in_reader.Read<uint>();
+            UnknownField11 = in_reader.Read<uint>();
+            UnknownField12 = in_reader.Read<float>();
+            UnknownField13 = in_reader.Read<float>();
             DistanceX = in_reader.Read<Distance<float>>();
             DistanceY = in_reader.Read<Distance<float>>();
             DistanceZ = in_reader.Read<Distance<float>>();
-            UnknownField44 = in_reader.Read<uint>();
+            UnknownField14 = in_reader.Read<uint>();
             Parameter = in_reader.Read<float>();
-            UnknownField46 = in_reader.Read<uint>();
+            UnknownField15 = in_reader.Read<uint>();
 
             var colourKeyFramesOffset = in_reader.Read<uint>();
 
@@ -192,21 +192,21 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
             PatternRows = in_reader.Read<int>();
             PatternIndex = in_reader.Read<uint>();
             PatternCount = in_reader.Read<uint>();
-            UnknownField58 = in_reader.ReadBoolean<uint>();
+            UnknownField16 = in_reader.ReadBoolean<uint>();
             UseRandomPattern = in_reader.ReadBoolean<uint>();
         }
 
         public void Write(BinaryObjectWriterEx in_writer)
         {
             in_writer.Write(EmitterType);
-            in_writer.Write(UnknownField2);
+            in_writer.Write(UnknownField1);
             in_writer.Write(Position);
             in_writer.WriteBoolean<uint>(IsCameraLocal);
             in_writer.Write(Radius);
             in_writer.Write(Scale);
             in_writer.Write(DistanceWidth);
             in_writer.Write(DistanceHeight);
-            in_writer.Write(UnknownField15);
+            in_writer.Write(UnknownField2);
             in_writer.WriteBoolean<uint>(UseSplineMotion);
             var splineKeyFramesOffset = in_writer.Reserve<uint>();
             in_writer.WriteBoolean<uint>(Loop);
@@ -214,26 +214,26 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
             in_writer.Write(BlendMode);
             in_writer.Write(ParticleCount);
             in_writer.Write(Rate);
-            in_writer.Write(UnknownField23);
-            in_writer.Write(UnknownField24);
+            in_writer.Write(UnknownField3);
+            in_writer.Write(UnknownField4);
             in_writer.Write(Angle);
             in_writer.Write(DistanceAngle);
             in_writer.WriteBoolean<uint>(Billboard);
-            in_writer.Write(UnknownField29);
-            in_writer.Write(UnknownField30);
-            in_writer.Write(UnknownField31);
-            in_writer.Write(UnknownField32);
-            in_writer.Write(UnknownField33);
-            in_writer.Write(UnknownField34);
-            in_writer.Write(UnknownField35);
-            in_writer.Write(UnknownField36);
-            in_writer.Write(UnknownField37);
+            in_writer.Write(UnknownField5);
+            in_writer.Write(UnknownField6);
+            in_writer.Write(UnknownField7);
+            in_writer.Write(UnknownField8);
+            in_writer.Write(UnknownField9);
+            in_writer.Write(UnknownField10);
+            in_writer.Write(UnknownField11);
+            in_writer.Write(UnknownField12);
+            in_writer.Write(UnknownField13);
             in_writer.Write(DistanceX);
             in_writer.Write(DistanceY);
             in_writer.Write(DistanceZ);
-            in_writer.Write(UnknownField44);
+            in_writer.Write(UnknownField14);
             in_writer.Write(Parameter);
-            in_writer.Write(UnknownField46);
+            in_writer.Write(UnknownField15);
             var colourKeyFramesOffset = in_writer.Reserve<uint>();
             in_writer.WriteBoolean<uint>(UseColorMotion);
             in_writer.WriteBoolean<uint>(UsePattern);
@@ -245,7 +245,7 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
             in_writer.Write(PatternRows);
             in_writer.Write(PatternIndex);
             in_writer.Write(PatternCount);
-            in_writer.WriteBoolean<uint>(UnknownField58);
+            in_writer.WriteBoolean<uint>(UnknownField16);
             in_writer.WriteBoolean<uint>(UseRandomPattern);
 
             in_writer.WriteReserved(splineKeyFramesOffset, (uint)in_writer.CalculateOffset(in_writer.Position, OffsetType.Relative), false);
