c7a89c772409b88e0d1d9185992ccd8f9bf2da68
Arg!! <66618295+Argx2121@users.noreply.github.com>
2026-03-23 00:54:38 +1030

    Added missing blend ops /  blend modes / logic ops (#27)
    * Added missing blendmodes
    
    * Added missing blendops
    
    * Added missing logicops

--- Marathon/Formats/Ninja/Flags/BlendOperation.cs
+++ Marathon/Formats/Ninja/Flags/BlendOperation.cs
@@ -8,6 +8,12 @@ namespace Marathon.Formats.Ninja.Flags
     [JsonConverter(typeof(StringEnumConverter))]
     public enum BlendOperation : uint
     {
-        NNE_BLENDOP_ADD = 0x8006
+        NNE_BLENDOP_ADD = 0x8006,
+        NNE_BLENDOP_MIN = 0x8007,
+        NNE_BLENDOP_MAX = 0x8008,
+        NNE_BLENDOP_SUB = 0x800A,
+        NNE_BLENDOP_REVSUB = 0x800B,
+        NNE_BLENDOP_REVSUBSIGNED = 0xF005,
+        NNE_BLENDOP_ADDSIGNED = 0xF006,
     }
 }
--- Marathon/Formats/Ninja/Flags/LogicOperation.cs
+++ Marathon/Formats/Ninja/Flags/LogicOperation.cs
@@ -8,6 +8,22 @@ namespace Marathon.Formats.Ninja.Flags
     [JsonConverter(typeof(StringEnumConverter))]
     public enum LogicOperation : uint
     {
-        NNE_LOGICOP_NONE = 0
+        NNE_LOGICOP_NONE = 0,
+        NNE_LOGICOP_CLEAR = 0x1500,
+        NNE_LOGICOP_AND = 0x1501,
+        NNE_LOGICOP_ANDREV = 0x1502,
+        NNE_LOGICOP_COPY = 0x1503,
+        NNE_LOGICOP_ANDINV = 0x1504,
+        NNE_LOGICOP_NOOP = 0x1505,
+        NNE_LOGICOP_XOR = 0x1506,
+        NNE_LOGICOP_OR = 0x1507,
+        NNE_LOGICOP_NOR = 0x1508,
+        NNE_LOGICOP_EQUIV = 0x1509,
+        NNE_LOGICOP_INVERT = 0x150A,
+        NNE_LOGICOP_ORREV = 0x150B,
+        NNE_LOGICOP_COPYINV = 0x150C,
+        NNE_LOGICOP_ORINV = 0x150D,
+        NNE_LOGICOP_NAND = 0x150E,
+        NNE_LOGICOP_SET = 0x150F,
     }
 }
