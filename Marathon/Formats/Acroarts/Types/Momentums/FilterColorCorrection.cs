72a21907182d17f201632b846ab22856740106f6
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-03-23 15:19:10 +0000

    FilterColorCorrection: named unknown fields

--- Marathon/Formats/Acroarts/Types/Momentums/FilterColorCorrection.cs
+++ Marathon/Formats/Acroarts/Types/Momentums/FilterColorCorrection.cs
@@ -1,17 +1,11 @@
-﻿using Marathon.Formats.Acroarts.Chunks;
-using Marathon.IO;
+﻿using Marathon.IO;
+using Marathon.IO.Types;
 
 namespace Marathon.Formats.Acroarts.Types.Momentums
 {
     public class FilterColorCorrection : IMomentumParamSet
     {
-        public float UnknownField1 { get; set; }
-
-        public float UnknownField2 { get; set; }
-
-        public float UnknownField3 { get; set; }
-
-        public float UnknownField4 { get; set; }
+        public Color<float, RGBA> Color { get; set; }
 
         public FilterColorCorrection() { }
 
@@ -22,18 +16,12 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
 
         public void Read(BinaryObjectReaderEx in_reader)
         {
-            UnknownField1 = in_reader.Read<float>();
-            UnknownField2 = in_reader.Read<float>();
-            UnknownField3 = in_reader.Read<float>();
-            UnknownField4 = in_reader.Read<float>();
+            Color = in_reader.Read<Color<float, RGBA>>();
         }
 
         public void Write(BinaryObjectWriterEx in_writer)
         {
-            in_writer.Write(UnknownField1);
-            in_writer.Write(UnknownField2);
-            in_writer.Write(UnknownField3);
-            in_writer.Write(UnknownField4);
+            in_writer.Write(Color);
         }
 
         public uint GetParamCount()
