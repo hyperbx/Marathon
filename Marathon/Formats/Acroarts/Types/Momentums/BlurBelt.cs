f77a62bd8f35699151c0707a567c80acff309a0a
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-03-23 14:46:54 +0000

    BlurBelt: named unknown fields

--- Marathon/Formats/Acroarts/Types/Momentums/BlurBelt.cs
+++ Marathon/Formats/Acroarts/Types/Momentums/BlurBelt.cs
@@ -1,4 +1,6 @@
 ﻿using Marathon.IO;
+using Marathon.IO.Types;
+using System.Numerics;
 
 namespace Marathon.Formats.Acroarts.Types.Momentums
 {
@@ -6,77 +8,53 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
     {
         public uint UnknownField1 { get; set; }
 
+        public float Start { get; set; }
+
+        public float End { get; set; }
+
         public float UnknownField2 { get; set; }
 
-        public float UnknownField3 { get; set; }
+        public int KeyFrameCount { get; set; }
 
-        public float UnknownField4 { get; set; }
+        public int VertexCount { get; set; }
+
+        public uint UnknownField3 { get; set; }
+
+        public uint UnknownField4 { get; set; }
+
+        public uint BlendMode { get; set; }
+
+        public uint NodeIndexStart { get; set; }
+
+        public Vector3 PositionStart { get; set; }
+
+        public Color<float, RGBA> ColorStart { get; set; }
+
+        public int FalloffTypeStart { get; set; }
+
+        public float FalloffPowerStart { get; set; }
 
         public uint UnknownField5 { get; set; }
 
+        public int NodeIndexEnd { get; set; }
+
+        public Vector3 PositionEnd { get; set; }
+
+        public Color<float, RGBA> ColorEnd { get; set; }
+
+        public int FalloffTypeEnd { get; set; }
+
+        public float FalloffPowerEnd { get; set; }
+
         public uint UnknownField6 { get; set; }
 
-        public uint UnknownField7 { get; set; }
+        public ColorApplyMode ColorApplyMode { get; set; }
 
-        public uint UnknownField8 { get; set; }
+        public float ApproachSpeed { get; set; }
 
-        public uint UnknownField9 { get; set; }
+        public Distance<float> BufferWidth { get; set; }
 
-        public uint UnknownField10 { get; set; }
-
-        public uint UnknownField11 { get; set; }
-
-        public float UnknownField12 { get; set; }
-
-        public float UnknownField13 { get; set; }
-
-        public float UnknownField14 { get; set; }
-
-        public uint UnknownField15 { get; set; }
-
-        public uint UnknownField16 { get; set; }
-
-        public float UnknownField17 { get; set; }
-
-        public uint UnknownField18 { get; set; }
-
-        public uint UnknownField19 { get; set; }
-
-        public uint UnknownField20 { get; set; }
-
-        public int UnknownField21 { get; set; }
-
-        public uint UnknownField22 { get; set; }
-
-        public float UnknownField23 { get; set; }
-
-        public float UnknownField24 { get; set; }
-
-        public float UnknownField25 { get; set; }
-
-        public float UnknownField26 { get; set; }
-
-        public uint UnknownField27 { get; set; }
-
-        public float UnknownField28 { get; set; }
-
-        public uint UnknownField29 { get; set; }
-
-        public uint UnknownField30 { get; set; }
-
-        public uint UnknownField31 { get; set; }
-
-        public uint UnknownField32 { get; set; }
-
-        public float UnknownField33 { get; set; }
-
-        public uint UnknownField34 { get; set; }
-
-        public float UnknownField35 { get; set; }
-
-        public uint UnknownField36 { get; set; }
-
-        public float UnknownField37 { get; set; }
+        public Distance<float> BufferHeight { get; set; }
 
         public BlurBelt() { }
 
@@ -88,83 +66,59 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
         public void Read(BinaryObjectReaderEx in_reader)
         {
             UnknownField1 = in_reader.Read<uint>();
+            Start = in_reader.Read<float>();
+            End = in_reader.Read<float>();
             UnknownField2 = in_reader.Read<float>();
-            UnknownField3 = in_reader.Read<float>();
-            UnknownField4 = in_reader.Read<float>();
+            KeyFrameCount = in_reader.Read<int>();
+            VertexCount = in_reader.Read<int>();
+            UnknownField3 = in_reader.Read<uint>();
+            UnknownField4 = in_reader.Read<uint>();
+            BlendMode = in_reader.Read<uint>();
+            NodeIndexStart = in_reader.Read<uint>();
+            PositionStart = in_reader.Read<Vector3>();
+            ColorStart = in_reader.Read<Color<float, RGBA>>();
+            FalloffTypeStart = in_reader.Read<int>();
+            FalloffPowerStart = in_reader.Read<float>();
             UnknownField5 = in_reader.Read<uint>();
+            NodeIndexEnd = in_reader.Read<int>();
+            PositionEnd = in_reader.Read<Vector3>();
+            ColorEnd = in_reader.Read<Color<float, RGBA>>();
+            FalloffTypeEnd = in_reader.Read<int>();
+            FalloffPowerEnd = in_reader.Read<float>();
             UnknownField6 = in_reader.Read<uint>();
-            UnknownField7 = in_reader.Read<uint>();
-            UnknownField8 = in_reader.Read<uint>();
-            UnknownField9 = in_reader.Read<uint>();
-            UnknownField10 = in_reader.Read<uint>();
-            UnknownField11 = in_reader.Read<uint>();
-            UnknownField12 = in_reader.Read<float>();
-            UnknownField13 = in_reader.Read<float>();
-            UnknownField14 = in_reader.Read<float>();
-            UnknownField15 = in_reader.Read<uint>();
-            UnknownField16 = in_reader.Read<uint>();
-            UnknownField17 = in_reader.Read<float>();
-            UnknownField18 = in_reader.Read<uint>();
-            UnknownField19 = in_reader.Read<uint>();
-            UnknownField20 = in_reader.Read<uint>();
-            UnknownField21 = in_reader.Read<int>();
-            UnknownField22 = in_reader.Read<uint>();
-            UnknownField23 = in_reader.Read<float>();
-            UnknownField24 = in_reader.Read<float>();
-            UnknownField25 = in_reader.Read<float>();
-            UnknownField26 = in_reader.Read<float>();
-            UnknownField27 = in_reader.Read<uint>();
-            UnknownField28 = in_reader.Read<float>();
-            UnknownField29 = in_reader.Read<uint>();
-            UnknownField30 = in_reader.Read<uint>();
-            UnknownField31 = in_reader.Read<uint>();
-            UnknownField32 = in_reader.Read<uint>();
-            UnknownField33 = in_reader.Read<float>();
-            UnknownField34 = in_reader.Read<uint>();
-            UnknownField35 = in_reader.Read<float>();
-            UnknownField36 = in_reader.Read<uint>();
-            UnknownField37 = in_reader.Read<float>();
+            ColorApplyMode = in_reader.Read<ColorApplyMode>();
+            ApproachSpeed = in_reader.Read<float>();
+            BufferWidth = in_reader.Read<Distance<float>>();
+            BufferHeight = in_reader.Read<Distance<float>>();
         }
 
         public void Write(BinaryObjectWriterEx in_writer)
         {
             in_writer.Write(UnknownField1);
+            in_writer.Write(Start);
+            in_writer.Write(End);
             in_writer.Write(UnknownField2);
+            in_writer.Write(KeyFrameCount);
+            in_writer.Write(VertexCount);
             in_writer.Write(UnknownField3);
             in_writer.Write(UnknownField4);
+            in_writer.Write(BlendMode);
+            in_writer.Write(NodeIndexStart);
+            in_writer.Write(PositionStart);
+            in_writer.Write(ColorStart);
+            in_writer.Write(FalloffTypeStart);
+            in_writer.Write(FalloffPowerStart);
             in_writer.Write(UnknownField5);
+            in_writer.Write(NodeIndexEnd);
+            in_writer.Write(PositionEnd);
+            in_writer.Write(ColorEnd);
+            in_writer.Write(FalloffTypeEnd);
+            in_writer.Write(FalloffPowerEnd);
             in_writer.Write(UnknownField6);
-            in_writer.Write(UnknownField7);
-            in_writer.Write(UnknownField8);
-            in_writer.Write(UnknownField9);
-            in_writer.Write(UnknownField10);
-            in_writer.Write(UnknownField11);
-            in_writer.Write(UnknownField12);
-            in_writer.Write(UnknownField13);
-            in_writer.Write(UnknownField14);
-            in_writer.Write(UnknownField15);
-            in_writer.Write(UnknownField16);
-            in_writer.Write(UnknownField17);
-            in_writer.Write(UnknownField18);
-            in_writer.Write(UnknownField19);
-            in_writer.Write(UnknownField20);
-            in_writer.Write(UnknownField21);
-            in_writer.Write(UnknownField22);
-            in_writer.Write(UnknownField23);
-            in_writer.Write(UnknownField24);
-            in_writer.Write(UnknownField25);
-            in_writer.Write(UnknownField26);
-            in_writer.Write(UnknownField27);
-            in_writer.Write(UnknownField28);
-            in_writer.Write(UnknownField29);
-            in_writer.Write(UnknownField30);
-            in_writer.Write(UnknownField31);
-            in_writer.Write(UnknownField32);
-            in_writer.Write(UnknownField33);
-            in_writer.Write(UnknownField34);
-            in_writer.Write(UnknownField35);
-            in_writer.Write(UnknownField36);
-            in_writer.Write(UnknownField37);
+            in_writer.Write(ColorApplyMode);
+            in_writer.Write(ApproachSpeed);
+            in_writer.Write(BufferWidth);
+            in_writer.Write(BufferHeight);
         }
 
         public uint GetParamCount()
