fb3ac1d60bcba060447c3b104ed1aefce601b46a
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-03-24 01:31:57 +0000

    ScaleRandomAdd: named unknown field

--- Marathon/Formats/Acroarts/Types/Momentums/ScaleRandomAdd.cs
+++ Marathon/Formats/Acroarts/Types/Momentums/ScaleRandomAdd.cs
@@ -9,11 +9,11 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
 
         public Vector3 Add { get; set; }
 
-        public uint UnknownField1 { get; set; }
+        public uint UnknownField { get; set; }
 
         public GTCounter GTCounter { get; set; }
 
-        public uint UnknownField2 { get; set; }
+        public bool UseUniformScale { get; set; }
 
         public ScaleRandomAdd() { }
 
@@ -26,18 +26,18 @@ namespace Marathon.Formats.Acroarts.Types.Momentums
         {
             Scale = in_reader.Read<Vector3>();
             Add = in_reader.Read<Vector3>();
-            UnknownField1 = in_reader.Read<uint>();
+            UnknownField = in_reader.Read<uint>();
             GTCounter = in_reader.Read<GTCounter>();
-            UnknownField2 = in_reader.Read<uint>();
+            UseUniformScale = in_reader.ReadBoolean<uint>();
         }
 
         public void Write(BinaryObjectWriterEx in_writer)
         {
             in_writer.Write(Scale);
             in_writer.Write(Add);
-            in_writer.Write(UnknownField1);
+            in_writer.Write(UnknownField);
             in_writer.Write(GTCounter);
-            in_writer.Write(UnknownField2);
+            in_writer.WriteBoolean<uint>(UseUniformScale);
         }
 
         public uint GetParamCount()
