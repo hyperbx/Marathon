2a0e3d7e4b3216c8194a21088b6c8b03c8ae96e0
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-04-22 16:53:58 +0100

    ArrayExtensions: use two lengths for 2D array creation instead of params

--- Marathon/Extensions/ArrayExtensions.cs
+++ Marathon/Extensions/ArrayExtensions.cs
@@ -11,20 +11,16 @@ namespace Marathon.Extensions
             /// Creates a two-dimensional jagged <see cref="Array"/> of the specified <see cref="Type"/> and length, with zero-based indexing.
             /// </summary>
             /// <typeparam name="T">The <see cref="Type"/> of the <see cref="Array"/> to create.</typeparam>
-            /// <param name="in_lengths">An array of 32-bit integers that represent the size of each dimension of the <see cref="Array"/> to create.</param>
+            /// <param name="in_lengthA">The size of the left <see cref="Array"/> to create.</param>
+            /// <param name="in_lengthB">The size of the right <see cref="Array"/> to create.</param>
             /// <returns>A new two-dimensional jagged <see cref="Array"/> of the specified <see cref="Type"/> with the specified length, using zero-based indexing.</returns>
             /// <exception cref="ArgumentNullException"/>
             /// <exception cref="ArgumentException"/>
             /// <exception cref="NotSupportedException"/>
             /// <exception cref="ArgumentOutOfRangeException"/>
-            public static T[][] CreateInstanceJagged2D<T>(params int[] in_lengths)
+            public static T[][] CreateInstanceJagged2D<T>(int in_lengthA, int in_lengthB = 0)
             {
-                ThrowHelper.ThrowArgumentNullException(nameof(in_lengths), in_lengths);
-
-                if (in_lengths.Length != 2)
-                    throw new ArgumentException("Exactly two lengths are required.");
-
-                return (T[][])CreateInstanceJagged<T>(in_lengths);
+                return (T[][])CreateInstanceJagged<T>([in_lengthA, in_lengthB]);
             }
 
             /// <summary>
