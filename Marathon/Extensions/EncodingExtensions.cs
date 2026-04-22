ce07587505919b4b7a1ab680edf2f075035e9643
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-04-22 16:48:11 +0100

    EncodingFactory -> EncodingExtensions, use static extension member

---
+++ Marathon/Extensions/EncodingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace Marathon.Extensions
+{
+    public static class EncodingExtensions
+    {
+        extension(Encoding)
+        {
+            public static Encoding ShiftJIS
+            {
+                get
+                {
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                    return Encoding.GetEncoding("shift-jis");
+                }
+            }
+        }
+    }
+}
